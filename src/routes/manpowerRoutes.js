359e42f266d69379229c165e2e74488aa2261d1f
LemonFruitz <52597213+LemonFruitz@users.noreply.github.com>
2026-07-11 04:27:50 +0900

    Update manpowerRoutes.js

--- src/routes/manpowerRoutes.js
+++ src/routes/manpowerRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { listManpower } = require('../controllers/manpowerController');
-const authMiddleware = require('../middleware/auth');
 
-router.get('/', authMiddleware, listManpower);
+router.get('/', listManpower);
 
 module.exports = router;
